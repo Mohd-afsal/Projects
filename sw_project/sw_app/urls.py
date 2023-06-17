62f74439c6d5a80bf4f98d1f28cd83fe992aa5e7
Mohd-afsal <mohammedafsal214@gmail.com>
2023-06-17 19:11:01 +0530

    replaced travel site folder

--- sw_project/sw_app/urls.py
+++ sw_project/sw_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.goto_index, name='index')
+    path('', views.goto_index, name='index'),
+    path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- sw_project/sw_app/views.py
+++ sw_project/sw_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
 from . models import resources
 
 # Create your views here.
@@ -7,3 +9,60 @@ from . models import resources
 def goto_index(request):
     sampl_img = resources.objects.all()
     return render(request, 'index.html', {'obj': sampl_img})
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Please check username & password")
+            return redirect(request, 'login')
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['confirm_password']
+        if username == '' or first_name == '' or last_name == '' or email == '' or password == '' or cpassword == '':
+            messages.info(request, "Please enter all fields")
+            return redirect('register')
+        else:
+            if password == cpassword:
+                if User.objects.filter(username=username).exists():
+                    messages.info(request, "Username is taken")
+                    return redirect('register')
+                elif User.objects.filter(email=email).exists():
+                    messages.info(request, "Email already registered")
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username,
+                                                    first_name=first_name,
+                                                    last_name=last_name,
+                                                    email=email,
+                                                    password=password)
+                    user.save()
+                    messages.info(request, "User Created")
+                    return redirect('login')
+            else:
+                messages.info(request, "Passwords not matching")
+                return redirect('register')
+    return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+def contact(request):
+    return render(request, 'contact.html')
